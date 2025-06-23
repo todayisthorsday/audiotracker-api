1ea900e439779a78291567ac6853e59a66cc4d05
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-23 21:21:16 -0230

    Add SessionRepo interface

--- src/main/java/com/audiotracker/audiotracker_api/repository/SessionRepo.java
+++ src/main/java/com/audiotracker/audiotracker_api/repository/SessionRepo.java
@@ -1,4 +1,7 @@
 package com.audiotracker.audiotracker_api.repository;
 
-public class SessionRepo {
+import com.audiotracker.audiotracker_api.model.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepo extends JpaRepository<Session, Long> {
 }
