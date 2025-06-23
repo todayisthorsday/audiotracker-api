904d9abfb2ae1cba734f401b42fff71b99927a0d
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-22 21:36:11 -0230

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/audiotracker/audiotracker_api/AudiotrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.audiotracker.audiotracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiotrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiotrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/audiotracker/audiotracker_api/AudiotrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audiotracker.audiotracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiotrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
