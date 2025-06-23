c9238bd012b92f1f93ecab7413ad7ff6cf827ac6
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-22 23:02:26 -0230

    Add Audiobook entity with Genre and Session relationships

---
+++ src/main/java/com/audiotracker/audiotracker_api/model/Audiobook.java
@@ -0,0 +1,84 @@
+package com.audiotracker.audiotracker_api.model;
+
+import jakarta.persistence.*;
+import jakarta.websocket.Session;
+
+import java.util.List;
+
+@Entity
+public class Audiobook {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private int duration; // MINUTES
+    private String narrator;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    @OneToMany(mappedBy = "audiobook", cascade = CascadeType.ALL)
+    private List<Session> sessions;
+
+    // GETTERS & SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getNarrator() {
+        return narrator;
+    }
+
+    public void setNarrator(String narrator) {
+        this.narrator = narrator;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+}
