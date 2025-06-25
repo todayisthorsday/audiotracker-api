ba8af699809abbc2aad40cab863d8aae61a21306
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-24 23:36:44 -0230

    add initial seed data for users, genres, audiobooks, and sessions

--- src/main/java/com/audiotracker/audiotracker_api/repository/UserRepo.java
+++ src/main/java/com/audiotracker/audiotracker_api/repository/UserRepo.java
@@ -3,5 +3,8 @@ package com.audiotracker.audiotracker_api.repository;
 import com.audiotracker.audiotracker_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
