7a51fe7d59a8832f1620de81c262f7297b9b29f1
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-27 23:31:55 -0230

    Merge pull request #21
    * Added custom endpoint to retrieve audiobooks by user ID using derived…

--- src/main/java/com/audiotracker/audiotracker_api/repository/AudiobookRepo.java
+++ src/main/java/com/audiotracker/audiotracker_api/repository/AudiobookRepo.java
@@ -3,5 +3,8 @@ package com.audiotracker.audiotracker_api.repository;
 import com.audiotracker.audiotracker_api.model.Audiobook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AudiobookRepo extends JpaRepository<Audiobook, Long> {
+    List<Audiobook> findBySessionsUserId(Long userId);
 }
