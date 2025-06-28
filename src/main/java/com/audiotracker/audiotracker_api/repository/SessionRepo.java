b8888bb88b30c747202df64ae73cc0ccb599d5f8
melanie <114537207+todayisthorsday@users.noreply.github.com>
2025-06-27 22:02:08 -0230

    Add custom query to fetch audiobooks by user ID in SessionRepo

--- src/main/java/com/audiotracker/audiotracker_api/repository/SessionRepo.java
+++ src/main/java/com/audiotracker/audiotracker_api/repository/SessionRepo.java
@@ -1,12 +1,22 @@
 package com.audiotracker.audiotracker_api.repository;
 
+import com.audiotracker.audiotracker_api.model.Audiobook;
 import com.audiotracker.audiotracker_api.model.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface SessionRepo extends JpaRepository<Session, Long> {
     @Query("SELECT SUM(s.length) FROM Session s WHERE s.user.id = :userId")
     Integer totalListeningByUser(@Param("userId") Long userId);
+    @Query("SELECT DISTINCT s.audiobook FROM Session s where s.user.id = :userId")
+    List<Audiobook> findAudiobooksByUserId(@Param("userId") Long userId);
+    // SessionRepo.java
+    @Query("SELECT s.audiobook FROM Session s WHERE s.user.id = :userId")
+    List<Audiobook> findAudiobooksByUserId(@Param("userId") Long userId);
 
 }
+
+
